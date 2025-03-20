5fe9927627d44b7a924ffb13b19ea53bd7f8cb98
sdnezz <twinknezz@gmail.com>
2025-03-21 00:22:49 +0300

    LAB5(2) - quadr solve

---
+++ rec/rec/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+type SolveResult = 
+    | None
+    | Linear of float
+    | Quadratic of float * float
+
+let solve a b c =
+    let D = b*b - 4.0*a*c
+    if a = 0.0 then
+        if b = 0.0 then None
+        else Linear(-c/b)
+    elif D = 0.0 then
+        Linear(-b / (2.0*a))
+    else 
+        if D < 0.0 then None
+        else Quadratic(((-b + sqrt D) / (2.0*a)),(-b - sqrt D) / (2.0*a))
+
+[<EntryPoint>]
+let main (args : string[]) = 
+    let res = solve 1.0 4.0 1.0
+    match res with 
+     | None -> System.Console.WriteLine("Нет решений")
+     | Linear(x) -> System.Console.WriteLine("Линейное уравнение, корень: {0}", x)
+     | Quadratic(x1, x2) -> System.Console.WriteLine("Квадратное уравнение, корни: {0} {1}", x1, x2)
+    0
