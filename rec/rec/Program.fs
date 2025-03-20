ab751271c59b3a3ce7b24d2d0532b35669ff805a
sdnezz <twinknezz@gmail.com>
2025-03-21 02:06:00 +0300

    LAB5(3.0) - cylindre valume (carry edition)

--- rec/rec/Program.fs
+++ rec/rec/Program.fs
@@ -15,6 +15,16 @@ let solve a b c =
         if D < 0.0 then None
         else Quadratic(((-b + sqrt D) / (2.0*a)),(-b - sqrt D) / (2.0*a))
 
+let circle_square r : float =
+    let pi = 3.14159
+    pi * r * r
+
+let cyl_volume h r : float =
+    h * circle_square r
+
+let cur_cyl_volume h = 
+    fun r -> cyl_volume h r
+
 [<EntryPoint>]
 let main (args : string[]) = 
     let res = solve 1.0 4.0 1.0
@@ -22,4 +32,10 @@ let main (args : string[]) =
      | None -> System.Console.WriteLine("Нет решений")
      | Linear(x) -> System.Console.WriteLine("Линейное уравнение, корень: {0}", x)
      | Quadratic(x1, x2) -> System.Console.WriteLine("Квадратное уравнение, корни: {0} {1}", x1, x2)
+    let r = 2.0
+    let h = 5.0
+    System.Console.WriteLine("Площадь круга: {0}", circle_square r)
+    System.Console.WriteLine("Объем цилиндра: {0}", cyl_volume h r)
+    let volumeFunc = cur_cyl_volume h
+    System.Console.WriteLine("Объем цилиндра через каррирование: {0}", volumeFunc r)
     0
