f256e48a078b8960decda3e2d824ad9253c8fc92
sdnezz <twinknezz@gmail.com>
2025-03-29 22:22:58 +0300

    LB6(0) - lists' of Chourch examples

---
+++ list/list/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+[<EntryPoint>]
+let main args =
+    let list1 = [2;3;4]
+    let list2 = 100::list1
+    System.Console.WriteLine(list2)
+    let list3 = list1 @ list2
+    System.Console.WriteLine("{0}", list2.Length)
+    System.Console.WriteLine("{0}", list2.Head)
+    System.Console.WriteLine("{0}", list2.Tail)
+    System.Console.WriteLine("{0}", list2.Tail.Head)
+    System.Console.WriteLine("{0}", list2.Tail.Tail.Head)
+    System.Console.WriteLine("{0}", list2.Tail.Tail.Head)
+    0
