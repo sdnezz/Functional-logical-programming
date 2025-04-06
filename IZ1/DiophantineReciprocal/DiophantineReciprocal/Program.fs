837751110da32932e8998fa7df783bf5275862a4
sdnezz <twinknezz@gmail.com>
2025-04-06 14:45:39 +0300

    IZ1 - Diophanite Reciprocal I problem: 1st step
    - число решений больше 1000 = число делителей n^2 + 1, деленное на 2, следовательно число делителей n^2 должно быть больше 1999
    - задача сводится к рекурсивному подбору показателей

---
+++ IZ1/DiophantineReciprocal/DiophantineReciprocal/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+// Список простых чисел (int64)
+let primes = [2L; 3L; 5L; 7L; 11L; 13L]
+
+// Фиксированные показатели для начала
+let exponents = [2; 2; 1; 1; 1; 1]
+
+// Вычисление n
+let n = 
+    List.zip primes exponents
+    |> List.map (fun (p, e) -> pown p e)
+    |> List.fold (*) 1L
+
+printfn "Минимальное n: %d" (int n)
