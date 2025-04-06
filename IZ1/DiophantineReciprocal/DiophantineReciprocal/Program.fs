36f36cd57709427c8ca5479fde10f26e804303fd
sdnezz <twinknezz@gmail.com>
2025-04-06 23:38:15 +0300

    Add calculateExponents for automatic exponent selection

--- IZ1/DiophantineReciprocal/DiophantineReciprocal/Program.fs
+++ IZ1/DiophantineReciprocal/DiophantineReciprocal/Program.fs
@@ -1,30 +1,39 @@
 ﻿open System
 
-// Список простых чисел (int64)
-let primes = [2L; 3L; 5L; 7L; 11L; 13L]
+let rec calculateExponents primes target currentProd currentExponents =
+    match primes with
+    | [] -> 
+        if currentProd > target then 
+            Some(currentExponents |> List.rev) 
+        else 
+            None
+    | p::rest ->
+        let maxExp = 
+            let remainingTarget = target / currentProd
+            if remainingTarget <= 1 then 
+                0
+            else 
+                (log (float remainingTarget) / log 3.0 |> ceil |> int)
+        
+        let rec tryExponent e =
+            if e > maxExp then 
+                None
+            else
+                let newProd = currentProd * (2 * e + 1)
+                match calculateExponents rest target newProd (e::currentExponents) with
+                | Some(exps) -> Some(exps)
+                | None -> tryExponent (e + 1)
+        
+        tryExponent 0
 
-// Целевое значение для d(n^2)
-let target = 1999
+// Оптимальные показатели степеней для минимального n
+let primes = [2L; 3L; 5L; 7L; 11L; 13L] // int64
+let exponents = [2; 2; 1; 1; 1; 1] // Показатели для n = 2^2 * 3^2 * 5^1 * 7^1 * 11^1 * 13^1
 
-// Функция для вычисления d(n^2) по списку показателей
-let d_n2 exponents =
-    exponents
-    |> List.map (fun e -> 2 * e + 1)
-    |> List.fold (*) 1
-
-// Фиксированные показатели для начала
-let exponents = [2; 2; 1; 1; 1; 1]
-
-// Вычисление n и проверка
+// Вычисление n с использованием pown для int64
 let n = 
     List.zip primes exponents
-    |> List.map (fun (p, e) -> pown p e)
+    |> List.map (fun (p, e) -> pown p e) // pown поддерживает int64
     |> List.fold (*) 1L
 
-// Вычисление d(n^2) и количества решений
-let divisors = d_n2 exponents
-let solutions = (divisors + 1) / 2
-
 printfn "Минимальное n: %d" (int n)
-printfn "d(n^2): %d" divisors
-printfn "Количество решений: %d" solutions
